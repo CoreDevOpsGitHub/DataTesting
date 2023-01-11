2beb41d14b7e53110f1bca439ebfea126030be8e
CoreDevOpsGitHub <64380295+CoreDevOpsGitHub@users.noreply.github.com>
2023-01-11 01:50:13 +0000

    adding more validations

--- DataTestSuite/Test_Steps/test_01_pilot_gas_pricing.py
+++ DataTestSuite/Test_Steps/test_01_pilot_gas_pricing.py
@@ -1,4 +1,4 @@
-from USER_INTERFFACE import show_entry_fields
+from USER_INTERFFACE import *
 
 import pyodbc as pyodbc
 from DataTestSuite.Drivers.set_up import fixed_driver
@@ -21,27 +21,39 @@ def test_pilot_gasstation_pricing(fixed_driver):
 def execute_query(fixed_driver):
     """The User Executes The Query"""
     global cursor
-    conx_string = pyodbc.connect("DRIVER={SQL Server};server=EC2AMAZ-O7K498H\SQLEXPRESS;database=sleepstudy;Trusted_Connection=yes;")
+    conx_string = pyodbc.connect(
+        "DRIVER={SQL Server};server=EC2AMAZ-O7K498H\SQLEXPRESS;database=sleepstudy;Trusted_Connection=yes;")
     cursor = conx_string.cursor()
 
 
 @given('The User Receives The Result Set', target_fixture="01_validate_data_test")
 def receive_result_set(fixed_driver):
     """The User Receives The Query Results"""
-   # global query_element
-    #exec = show_entry_fields()
-    exec = show_entry_fields().expected_value_string
-    print(exec)
-    #print(show_entry_fields())
-   # cursor.execute()
-   # query_result = cursor.fetchone()
-   # for query_element in query_result:
-   #     print(query_element)
+    # global query_element
+    # exec = show_entry_fields()
+    # exec = show_entry_fields().expected_value_string
+    # print(exec)
+    # print(show_entry_fields())
+
+
+# cursor.execute()
+# query_result = cursor.fetchone()
+# for query_element in query_result:
+#     print(query_element)
 
 
 @then("Confirm the Results Matches Expected Output", target_fixture="01_validate_data_test")
 def validate_result_set(fixed_driver):
     """The User Validates The Results Are As Expected"""
-    #print(query_element)
-    #print(expected_value_string)
-    #assert expected_value_string == query_element, "Both are Matching"
+
+
+    query_string_value_1 = App().submit()
+    print("Value" + query_string_value_1)
+
+    #def queryStringValue(self):
+     #   self.obj = App().query_string_value
+      #  return self.obj
+
+# print(query_element)
+# print(expected_value_string)
+# assert expected_value_string == query_element, "Both are Matching"
